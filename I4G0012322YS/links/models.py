6a03b5192e682583694c50fd6dc6d9ae42b1948b
angel-nico <71854636+angel-nico@users.noreply.github.com>
2022-07-14 17:25:19 +0100

    changes

--- I4G0012322YS/links/models.py
+++ I4G0012322YS/links/models.py
@@ -4,5 +4,15 @@ from django.db import models
 
 # Create your models here.
 
-class linksmodel(link):
+
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Link(models.Model):
     target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(blank=True, unique=True, max_length=20)
+    
