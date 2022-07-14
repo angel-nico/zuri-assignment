a4e8b5b007809a32158c800578af4605a0e15c23
angel-nico <71854636+angel-nico@users.noreply.github.com>
2022-07-14 19:08:30 +0100

    changes

--- I4G0012322YS/links/models.py
+++ I4G0012322YS/links/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
@@ -15,4 +16,8 @@ class Link(models.Model):
     target_url = models.URLField(max_length=200)
     description = models.CharField(max_length=200)
     identifier = models.SlugField(blank=True, unique=True, max_length=20)
-    
+    author = get_user_model
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+
